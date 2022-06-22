e59e7e43751cd238b93d0249893078072be80cde
amasson42 <giantwow3896@gmail.com>
2022-06-23 01:05:31 +0200

    game of life ready

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+all: build
+
+build:
+	wasm-pack build
+
+run:
+	cd www && npm run start
--- www/index.js
+++ www/index.js
@@ -1,3 +1,19 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+var ignoring = 3;
+const renderLoop = () => {
+    pre.textContent = universe.render();
+
+    ignoring -= 1;
+    if (ignoring < 0) {
+        ignoring = 3;
+        universe.tick();
+    }
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
