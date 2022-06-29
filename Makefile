09b3a97e740f6e46ec61f0084aa90606f3f5cf04
amasson42 <arthur.masson.3896@gmail.com>
2022-06-29 17:33:07 +0200

    Testable

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
 
 all: build
 
+setup:
+# apt install build-essential nodejs npm
+	@cargo version || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	@wasm-pack -V || curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+	@cargo generate -V || cargo install cargo-generate
+
 build:
 	wasm-pack build
+	cd www && npm install
 
 run:
 	cd www && npm run start
+
+test:
+	wasm-pack test --firefox --headless
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
@@ -41,39 +41,73 @@ impl Universe {
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
         for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width -  1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
                 if delta_row == 0 && delta_col == 0 {
                     continue;
                 }
 
                 let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col =  (column + delta_col) % self.width;
+                let neighbor_col = (column + delta_col) % self.width;
                 let idx = self.get_index(neighbor_row, neighbor_col);
                 count += self.cells[idx] as u8;
             }
         }
         count
     }
+
+    fn make_cells(&self) -> Vec<Cell> {
+        (0..self.width * self.height).map(|_i| Cell::Dead).collect()
+    }
+}
+
+fn make_line_cells(width: u32, height: u32) -> Vec<Cell> {
+    (0..width * height)
+        .map(|i| {
+            if i % 2 == 0 || i % 7 == 0 {
+                Cell::Alive
+            } else {
+                Cell::Dead
+            }
+        })
+        .collect()
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, column) in cells.iter().cloned() {
+            let idx = self.get_index(row, column);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 #[wasm_bindgen]
 impl Universe {
-
     pub fn new() -> Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let cells = make_line_cells(width, height);
 
-            Universe { width, height, cells }
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = self.make_cells();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = self.make_cells();
     }
 
     pub fn tick(&mut self) {
